94a37c370faacbe3e683e80921851019f0a30c38
sofia <sofia.strygina@gmail.com>
2026-08-04 08:06:12 +0300

    Ипсравлена вывод ошибки

--- server/BookCatalog.Api/Validation/BookInputValidator.cs
+++ server/BookCatalog.Api/Validation/BookInputValidator.cs
@@ -52,7 +52,7 @@ public static class BookInputValidator
 
         if (input.Pages is { } pages && (pages < 1 || pages > 10000))
         {
-            errors[nameof(input.Pages)] = ["Страниц должно быть не больше 10000"];
+            errors[nameof(input.Pages)] = ["Страниц должно быть не больше 10000 и не меньше 1"];
         }
 
         return errors;
